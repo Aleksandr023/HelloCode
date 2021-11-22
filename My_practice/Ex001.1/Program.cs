6102f206bb83b2cca59a1367d4b0e42456858d83
AVoit <AVoit@myemail.com>
2021-11-22 23:18:01 +0500

    add ex 001.1

---
+++ My_practice/Ex001.1/Program.cs
@@ -0,0 +1,15 @@
+﻿// проверяем является ли numberA квадратом numberB
+
+int numberA = 49;
+int numberB = 7;
+
+int result = numberA/numberB;
+
+if(result == numberB)
+{
+    Console.WriteLine("Yes");
+}
+else 
+{
+    Console.WriteLine("No");
+}
