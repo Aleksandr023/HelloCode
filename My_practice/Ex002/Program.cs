326f5fd1172213674e06a593770171b5c29ef04c
AVoit <AVoit@myemail.com>
2021-11-22 23:50:37 +0500

    add ex 002 показать большее и меньшее число

---
+++ My_practice/Ex002/Program.cs
@@ -0,0 +1,21 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+int numberA = 45;
+int numberB = 19;
+
+if (numberA > numberB)
+{
+    Console.Write("Большее число ");
+    Console.WriteLine(numberA);
+
+    Console.Write("Меньшее число ");
+    Console.WriteLine(numberB);
+}
+else
+{
+    Console.Write("Большее число ");
+    Console.WriteLine(numberB);
+    
+    Console.Write("Меньшее число ");
+    Console.WriteLine(numberA);
+}
