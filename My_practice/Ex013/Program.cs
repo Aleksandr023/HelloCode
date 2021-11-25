d1cad1511c86cb459293b4475a2e9c3d056b64f7
AVoit <AVoit@myemail.com>
2021-11-25 23:59:16 +0500

    Выяснить, кратно ли число заданному, если нет, вывести остаток

---
+++ My_practice/Ex013/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+int N = new Random().Next(1, 100);
+Console.WriteLine(N);
+Console.WriteLine("Введите своё число");
+int A = int.Parse(Console.ReadLine());
+
+if(A%N==0)
+{
+    Console.WriteLine("OK");
+}
+else 
+{
+    Console.WriteLine(A%N);  // % - покажет нам остаток от деления
+}
