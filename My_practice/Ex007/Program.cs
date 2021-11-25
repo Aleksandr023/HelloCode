d67057c3e67e40dac8b80cad87106f3bcb47b4a9
AVoit <AVoit@myemail.com>
2021-11-26 01:46:30 +0500

    Ex007 добавил возможность рабр=оты с отрицательными числами

--- My_practice/Ex007/Program.cs
+++ My_practice/Ex007/Program.cs
@@ -2,10 +2,23 @@
 
 Console.WriteLine("Введите любое число");
 int N = int.Parse(Console.ReadLine());
-int X = -N;
-while (X <= N)
+if (N > 0)
 {
-    Console.Write(X);
-    Console.Write(" ");
-    X++;
+    int X = -N;
+    while (X <= N)
+    {
+        Console.Write(X);
+        Console.Write(" ");
+        X++;
+    }
+}
+else
+{
+    int X = N;
+    while (X <= N*-1)
+    {
+       Console.Write(X);
+       Console.Write(" ");
+       X++; 
+    }
 }
