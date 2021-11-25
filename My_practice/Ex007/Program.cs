05059aa7b9e371f92130f275cd3057910685be43
AVoit <AVoit@myemail.com>
2021-11-25 23:13:00 +0500

    -N till N, 1 - N %2

---
+++ My_practice/Ex007/Program.cs
@@ -0,0 +1,5 @@
+﻿//Показать числа от -N до N
+int n = int.Parse(Console.ReadLine());
+int [] array = {};
+array.Length = 2*n+1;
+Console.WriteLine(array);
---
+++ My_practice/Ex008/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать четные числа от 1 до N
+
+Console.WriteLine("Введите значение N");
+int N = int.Parse(Console.ReadLine());
+
+int result = 1;
+while (result<N+1)
+{
+    if (result%2 == 0)
+    {
+        Console.Write(result); 
+        Console.Write(" ");
+    }
+    result++;
+}
