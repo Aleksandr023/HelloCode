7bce52becf600f8e8884f5560e6df756df11e2ca
AVoit <AVoit@myemail.com>
2021-11-26 00:49:13 +0500

    Найти третью цифру числа или сообщить, что её нет. Познакомился с методом ToStrung()

---
+++ My_practice/Ex014/Program.cs
@@ -0,0 +1,21 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+int N = new Random().Next(-30000, 30000);
+Console.WriteLine(N);
+if (N >= -99 && N <= 99)
+{
+    Console.WriteLine("Третья цифра отсутствует");
+}
+else
+{
+    if (N < -99)
+    {
+        Console.WriteLine(N.ToString()[3]); // ToString() - преобразовывает объект в строку, + [1] - выводит элемент с индексом 1
+    }
+    else
+    {
+        Console.WriteLine(N.ToString()[2]);
+    }
+    
+}
+
