c17fa3395e1dfa1ad51f13692719af352b6919b2
AVoit <AVoit@myemail.com>
2021-11-26 01:17:43 +0500

    Проверка кратности двум числам

---
+++ My_practice/Ex015/Program.cs
@@ -0,0 +1,21 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+int N = new Random().Next(-1000, 1001);
+if (N%7==0)
+{
+    if (N%23==0)
+    {
+        Console.Write(N);
+        Console.Write(" кратно 7 и 23");
+    }
+    else
+    {
+        Console.Write(N);
+        Console.Write(" не кратно 7 и 23");
+    }
+}
+else
+{
+    Console.Write(N);
+    Console.Write(" не кратно 7 и 23");
+}
