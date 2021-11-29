b5d18ba3d951f9856b6e76e3ef3a3bc3b1073e12
AVoit <AVoit@myemail.com>
2021-11-30 00:19:51 +0500

    Ex022 ++

--- My_practice/Ex022/Program.cs
+++ My_practice/Ex022/Program.cs
@@ -1,12 +1,14 @@
 ﻿// Найти расстояние между точками в пространстве 2D/3D
 
-int X1 = 4;
-int Y1 = 7;
-int X2 = 5;
-int Y2 = 1;
-int Z1 = 18;
-int Z2 = 16;
+double X1 = new Random().Next(-15, 16);
+double Y1 = new Random().Next(-15, 16);
+double X2 = new Random().Next(-15, 16);
+double Y2 = new Random().Next(-15, 16);
+double Z1 = new Random().Next(-15, 16);
+double Z2 = new Random().Next(-15, 16);
+
+Console.WriteLine("Координаты точки A " + (X1, Y1, Z1) + ", координаты точки B " + (X2, Y2, Z2));
 
 Console.WriteLine("Расстояние между точками в пространстве 2D " + Math.Sqrt((Math.Pow(X2-X1, 2)+Math.Pow(Y2-Y1, 2))));
 
-Console.WriteLine("Расстояние между двумя точками в пространстве 3D " + Convert.ToInt32(Math.Sqrt((Math.Pow(X2-X1, 2)+Math.Pow(Y2-Y1, 2)+Math.Pow(Z2-Z1,2)))));
+Console.WriteLine("Расстояние между двумя точками в пространстве 3D " + Math.Sqrt((Math.Pow(X2-X1, 2)+Math.Pow(Y2-Y1, 2)+Math.Pow(Z2-Z1,2))));
