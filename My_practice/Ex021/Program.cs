8d56b8734a16c35d0ecae96b7147b6e03da01328
AVoit <AVoit@myemail.com>
2021-11-29 23:41:56 +0500

    Ex020

---
+++ My_practice/Ex021/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа проверяет пятизначное число на палиндромом
+
+Console.WriteLine("Введите пятизначное число");
+int number = int.Parse(Console.ReadLine());
+
+if(number/10000 == number%10 && number/1000%10 == number%100/10)
+{
+    Console.WriteLine("Число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Число не является палиндромом");
+}
