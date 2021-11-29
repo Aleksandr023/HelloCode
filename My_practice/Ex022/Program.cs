86f1d938277eb4515d3f17120f1ae647d3866ea5
AVoit <AVoit@myemail.com>
2021-11-30 00:05:03 +0500

    Ex022

---
+++ My_practice/Ex022/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти расстояние между точками в пространстве 2D/3D
+
+int X1 = 4;
+int Y1 = 7;
+int X2 = 5;
+int Y2 = 1;
+int Z1 = 18;
+int Z2 = 16;
+
+Console.WriteLine("Расстояние между точками в пространстве 2D " + Math.Sqrt((Math.Pow(X2-X1, 2)+Math.Pow(Y2-Y1, 2))));
+
+Console.WriteLine("Расстояние между двумя точками в пространстве 3D " + Convert.ToInt32(Math.Sqrt((Math.Pow(X2-X1, 2)+Math.Pow(Y2-Y1, 2)+Math.Pow(Z2-Z1,2)))));
