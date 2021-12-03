f47c47427a25a0c2d7433182c994fbd3bcd759a6
aaa <aaa@aaa.com>
2021-12-03 15:05:49 +0500

    ex028 Подсчитать сумму цифр в числе

---
+++ My_practice/Ex028/Program.cs
@@ -0,0 +1,10 @@
+﻿// Подсчитать сумму цифр в числе
+
+int number = int.Parse(Console.ReadLine());
+int summ = 0;
+while (number > 0)
+{
+    summ = summ + number%10;
+    number = number/10;
+}
+Console.WriteLine(summ);
