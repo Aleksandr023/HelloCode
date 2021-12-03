03713a076f2144cf5cdd6fb5bdb11ce31afcda55
aaa <aaa@aaa.com>
2021-12-03 13:27:13 +0500

    ex025 Найти сумму чисел от 1 до А

---
+++ My_practice/Ex025/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму чисел от 1 до А
+
+
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+int result = 0;
+for (int i = 1; i <= A; i++)
+{
+      result = result + i;
+}
+Console.WriteLine(result);
