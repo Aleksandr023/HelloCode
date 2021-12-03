03e239d029b1df180255b134cf9836f7e4fecb5b
aaa <aaa@aaa.com>
2021-12-03 13:15:44 +0500

    ex023 таблица квадратов чисел 1 - n

---
+++ My_practice/Ex023/Program.cs
@@ -0,0 +1,9 @@
+﻿//Показать таблицу квадратов чисел от 1 до N
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine($"{i} x {i} = {i*i}");    
+    Console.WriteLine();
+}
