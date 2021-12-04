2663d76bb3faf81c0737a29ce78b871e00a231b6
AVoit <AVoit@myemail.com>
2021-12-04 22:45:05 +0500

    Ex031 задать массив из 8 элементов и вывести их на экран

---
+++ My_practice/Ex031/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+
+void FillArray(int[] array)
+{
+    int length= array.Length;
+    int index = 0;
+    while (index<length)
+    {
+        array[index] = new Random().Next(0, 99);
+        index++;
+    }
+}
+
+void PrintArray(int[] newArray)
+{
+    int count = newArray.Length;
+    int position = 0;
+    while (position < newArray.Length)
+    {
+        Console.Write($"{newArray[position]} ");
+        position++;
+    }
+}
+
+int[] numbers = new int [8];
+
+FillArray(numbers);
+PrintArray(numbers);
