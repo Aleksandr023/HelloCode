c457d9791fc984d81657630d028829a279d2df61
AVoit <AVoit@myemail.com>
2021-12-04 22:05:45 +0500

    ex023

---
+++ My_practice/Ex023/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать таблицу квадратов чисел от 1 до N 
+ class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите кол-во чисел для вывода:");
+            int N = int.Parse(Console.ReadLine());
+ 
+            Console.WriteLine("Число" + "\t" + "Квадрат");
+            for(int i = 1; i <= N; i++)
+                Console.WriteLine(i + "\t" + i*i);
+ 
+            Console.ReadLine();
+  
+        }
+    }
