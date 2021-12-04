7da795a292d8dc1e351a502916eb34e7521dbc58
AVoit <AVoit@myemail.com>
2021-12-05 00:18:35 +0500

    Ex035

---
+++ My_practice/Ex035/Program.cs
@@ -0,0 +1,48 @@
+﻿// Определить, присутствует ли в заданном массиве, некоторое число
+void FillArray(int[] array)
+{
+    int index = 0;
+    int length = array.Length;
+    while (index < length)
+    {
+        array[index] = new Random().Next(1, 99);
+        index++;
+    }
+}
+
+void PrintArray(int[] numbers)
+{
+    int count = 0;
+    int length = numbers.Length;
+    while (count < length)
+    {
+        Console.Write($"{numbers[count]} ");
+        count++;
+    }
+    Console.WriteLine();
+}
+
+void FindNumber(int[] array)
+{
+    int length = array.Length;
+    Console.Write("Введите искомое число: ");
+    int number = int.Parse(Console.ReadLine());
+    for (int index = 0; index < length; index++)
+    {
+        if (number == array[index])
+        {
+            Console.WriteLine($"Число {number} присутствует в массиве под индексом {index}");
+            break;
+        }
+    }        
+}    
+
+int[] testarray = new int [15];
+
+FillArray(testarray);
+PrintArray(testarray);
+FindNumber(testarray);
+
+
+
+
